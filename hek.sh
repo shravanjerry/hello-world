5277ec54368accdc83a4bd41164221401baa1f9a
shravanjerry <66510252+shravanjerry@users.noreply.github.com>
2020-06-15 15:07:32 +0530

    Update hek.sh

--- hek.sh
+++ hek.sh
@@ -1,13 +1,1 @@
-sleep 1 echo -n "What is your name? Type it:" read nama; 
-sleep 2 echo "Hi hello this is MrWHITEHATTER \033[036m[$nama]" sleep 1 
-echo "\033[037mJangan Lupa subscribe channel admin woke:)" 
-sleep 1 
-clear echo -n "did you subscribed my channel MrWHITEHATTER TECH? y/n :" read pilih if [ $pilih = "y" ]; 
-then 
-sleep 2 echo "PLZ SUBSCRIBE My youtube CHANNEL MrWHITEHATTER TECH" termux-open-url "https://www.youtube.com/channel/UCw5u7r2zZOJgPSe-3qyuOag"
-sleep 2 
-clear elif [ $pilih = "n" ]; 
-then 
-sleep 2 
-echo "PLZ SUBSCRIBE My youtube CHANNEL MrWHITEHATTER TECH" termux-open-url "https://www.youtube.com/channel/UCw5u7r2zZOJgPSe-3qyuOag" sleep 4 
-echo "MAKASIH SUDAH DUKUNG CHANNEL ADMIN^_^" sleep 2 clear else echo "anda salah memasukan input!!!!" sleep 2 sh
+limit=100sleep 1echo "\033[031m ▇▇▇▇▇▇▇"echo "\033[037m ▇▇▇▇▇▇▇"echo "\033[031mSELAMAT DATANG \033[037mDi Tools Apmz"sleep 1echo -n "what is your Name?type it:"read Name;sleep 2echo "Hello Mr/ Mrs \033[036m[$nama]"sleep 1echo "\033[037mJangan Lupa subscribe channel admin woke:)"sleep 1clearecho -n "Did you subscribed my youtube channel ? y/n :"read pilihif [ $pilih = "y" ];thensleep 2echo "Thanks for subscribing my channel"sleep 2clearelif [ $pilih = "n" ];thensleep 2echo "Please SUBSCRIBE CHANNEL My channel"termux-open-url "https://www.youtube.com/channel/UCw5u7r2zZOJgPSe-3qyuOag "sleep 4echo "MAKASIH SUDAH DUKUNG CHANNEL ADMIN^_^"sleep 2clearelseecho "anda salah memasukan input!!!!"sleep 2sh hek.shficlearsleep 1python2 loding692sleep 1clear
