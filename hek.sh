a8fb3d0ef6565ce3ff80ed0334fd04230cc72445
shravanjerry <66510252+shravanjerry@users.noreply.github.com>
2020-06-15 17:04:03 +0530

    Update hek.sh

--- hek.sh
+++ hek.sh
@@ -5,7 +5,7 @@ sleep 1
 echo -n "what is your Name?:"
 read Name;
 sleep 2
-echo "Hello Mr/ Mrs \033[036m[$Name]"
+echo "Hello Mr/ Mrs [$Name]"
 sleep 1
 clear
 echo -n "Did you subscribed my youtube channel ? y/n :"
@@ -22,12 +22,6 @@ sleep 2
 echo "Please SUBSCRIBE CHANNEL My channel"
 termux-open-url "https://www.youtube.com/channel/UCw5u7r2zZOJgPSe-3qyuOag "
 sleep 4
-echo "MAKASIH SUDAH DUKUNG CHANNEL ADMIN^_^"
 sleep 2
-clear
-else
-echo "anda salah memasukan input!!!!"
-sleep 2
-sh hek.sh
 fi
 clear
